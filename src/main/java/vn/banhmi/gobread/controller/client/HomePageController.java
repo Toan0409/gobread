adde9c6554ef972e596b95f9e1c3977ba4eaf4f2
Toan0409 <Tranquoctoan04092004@gmail.com>
2025-05-27 09:58:08 +0700

    thêm folder controler/client

--- src/main/java/vn/banhmi/gobread/controller/client/HomePageController.java
+++ src/main/java/vn/banhmi/gobread/controller/client/HomePageController.java
@@ -1,15 +1,28 @@
 package vn.banhmi.gobread.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import vn.banhmi.gobread.domain.Product;
+import vn.banhmi.gobread.service.ProductService;
+
 @Controller
 public class HomePageController {
 
+    private final ProductService productService;
+
+    public HomePageController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/user")
     public String getHomeUserPage(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
         return "client/home/index";
     }
 
